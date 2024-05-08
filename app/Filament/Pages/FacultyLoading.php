681b80d6e715c3c0ee056bf569a5630a211917cf
lacnnea <clarissebalaoro06@gmail.com>
2024-05-08 15:48:50 +0800

    facultyloading, facultyrecords, facultydesignation, studyload

--- app/Filament/Pages/FacultyLoading.php
+++ app/Filament/Pages/FacultyLoading.php
@@ -2,13 +2,34 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\Aysem;
+use App\Models\TaClass;
+use Filament\Forms\Components;
+use Filament\Forms\Form;
 use Filament\Pages\Page;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Tables; // Add this line
 
-class FacultyLoading extends Page
+class FacultyLoading extends Page implements HasForms
 {
+    use InteractsWithForms;
+
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.faculty-loading';
 
     protected static ?string $navigationGroup = 'Print Forms';
+
+    public function form(Form $form): Form
+    {
+        return $form->columns(1)->schema([
+            Components\Select::make('Select')
+                ->placeholder('AYSEM')
+                ->options(Aysem::pluck('year')) // Assuming Faculty model has 'name' and 'id' fields
+                ->required(),
+        ]);
+    }
+
+
 }
