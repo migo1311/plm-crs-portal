211ba56b5f933f5716374af94188130ad54fb1cf
liza-liz <elizalim036@gmail.com>
2024-05-20 18:44:28 +0800

    added student term

---
+++ app/Models/StudentTerm.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentTerm extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'student_term_id';
+
+    protected $fillable = [
+        'student_id',
+        'academic_year',
+        'semester',
+        'aysem_id',
+        'college_id',
+        'program_id',
+        'block_id',
+        'student_type',
+        'registration_status',
+        'graduating',
+        'enrolled',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class, 'student_id', 'student_id');
+    }
+
+    public function aysem()
+    {
+        return $this->belongsTo(Aysem::class, 'aysem_id', 'aysem_id');
+    }
+
+    public function college()
+    {
+        return $this->belongsTo(College::class, 'college_id', 'college_id');
+    }
+
+    public function program()
+    {
+        return $this->belongsTo(Program::class, 'program_id', 'program_id');
+    }
+
+    public function block()
+    {
+        return $this->belongsTo(Block::class, 'block_id', 'block_id');
+    }
+}
