f637688ef1bed350296e4ba1b4ea4fb1069d137f
liza-liz <elizalim036@gmail.com>
2024-05-21 23:34:19 +0800

    created faculty panel and pages

---
+++ app/Filament/Faculty/Pages/TeachingAssignment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Faculty\Pages;
+
+use Filament\Pages\Page;
+
+class TeachingAssignment extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.faculty.pages.teaching-assignment';
+}
--- resources/views/filament/pages/student-records.blade.php
+++ resources/views/filament/pages/student-records.blade.php
@@ -15,28 +15,4 @@
         </div>
     </form>
 
-
-    <!-- @if ($data)
-        <div class="bg-white p-6 mt-6 rounded-md shadow-sm">
-            <h2 class="text-xl font-semibold mb-4">Faculty Records</h2>
-            <div style="overflow-x: auto;">
-                <table class="min-w-full bg-white">
-                    <thead>
-                        <tr>
-                            @foreach(array_keys($data) as $field)
-                                <th class="px-4 py-2 border">{{ ucwords(str_replace('_', ' ', $field)) }}</th>
-                            @endforeach
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            @foreach($data as $value)
-                                <td class="px-4 py-2 border">{{ $value }}</td>
-                            @endforeach
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    @endif -->
 </x-filament-panels::page>
