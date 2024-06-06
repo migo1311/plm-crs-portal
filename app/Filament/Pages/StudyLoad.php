93799774e3e678686a61e2b433ff699d9e59590f
liza-liz <elizalim036@gmail.com>
2024-06-06 08:55:11 +0800

    able to submit

--- app/Filament/Pages/StudyLoad.php
+++ app/Filament/Pages/StudyLoad.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\Aysem;
 use App\Models\Instructor;
 use Filament\Forms\Components;
 use Filament\Forms\Form;
@@ -44,14 +45,16 @@ class StudyLoad extends Page implements HasForms
         ->columns(1)
         ->schema([
             Components\Select::make('instructor_id')
-                ->placeholder('Select Faculty Name')
-                ->options(Instructor::pluck('id', 'id')) // Assuming Faculty model has 'name' and 'id' fields
+                ->label('Select Faculty Name')
+                ->options(Instructor::pluck('faculty_name', 'id')) // Assuming Faculty model has 'name' and 'id' fields
                 ->required(),
             Components\TextInput::make('study_units')
                 ->placeholder('Study Load (Units)')
+                ->maxLength(2)
                 ->required(),
             Components\TextInput::make('teaching_units')
                 ->placeholder('Outside Teaching Load (Units)')
+                ->maxLength(2)
                 ->required(),
         ]);
     }
@@ -60,16 +63,14 @@ class StudyLoad extends Page implements HasForms
     {
         $data = $this->form->getState();
         $userId = Auth::id(); // Get the ID of the authenticated user
-
-        DB::transaction(function() use ($data, $userId) {
-            StudyLoadModel::create([
-                'instructor_id' => $data['instructor_id'],
-                'study_units' => $data['study_units'],
-                'teaching_units' => $data['teaching_units'],
-                'entered_by' => $userId,
-                'entered_on' => now() // Set the 'entered_by' field to the authenticated user's ID
-            ]);
-        });
+        StudyLoadModel::create([
+            'instructor_id' => $data['instructor_id'],
+            'study_units' => $data['study_units'],
+            'teaching_units' => $data['teaching_units'],
+            'aysem_id' => Aysem::all()->last()->getAttributeValue('id'),
+            'entered_by' => $userId,
+            'entered_on' => now() // Set the 'entered_by' field to the authenticated user's ID
+        ]);
 
         session()->flash('success', 'Faculty Load submitted.');
     }
